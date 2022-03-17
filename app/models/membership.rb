0c6d93fd96a4ce8f35e9807934912af8337c120d
elainezhang <ezzhang@smith.edu>
2022-03-17 11:33:32 -0700

    update membership class attributes

---
+++ app/models/membership.rb
@@ -0,0 +1,11 @@
+class Membership < ApplicationRecord
+    validates_presence_of    :identifier,
+                             :name,
+                             :cycle_length,
+                             :start_date
+    validates_uniqueness_of  :identifier
+    
+    has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
+    
+  end
+   
