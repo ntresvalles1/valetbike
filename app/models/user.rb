300cfe438068523ef2b7e3aee188c799c68cb03c
elainezhang <ezzhang@smith.edu>
2022-03-27 12:10:46 -0400

    update user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# User Model
+# add total_rides attribute --> a list of Rides
+# rides_this_month attribute -> list of Rides, refreshes every month
+
 class User < ApplicationRecord
-end
+    attr_reader :user_id 
+    attr_accessor :name, :phone, :password, :email, :cash
+
+    def initialize(user_id, name, phone, password, email, cash)
+        @user_id = user_id
+        @name = name
+        @phone = phone
+        @password = password
+        @email = email
+        @cash = cash
+   end
+
+end 
