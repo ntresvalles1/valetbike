86f34f481ab8654cf97eb46ba709f777e23388ca
Mariem Snoussi <76986301+mariem-snoussi@users.noreply.github.com>
2022-04-04 22:28:13 -0400

    cleaned comments

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,40 +1,10 @@
 class StationsController < ApplicationController
-  
-  # GET /stations
-  # GET /stations.json
-
 
   helper_method :index
 
   def index
     @stations = Station.all
-
-    #render json: @stations
-
-    
-    #respond_to do |format|
-      #format.html
-      #format.json do
-
-        #puts "please?"
-        #geojson = @stations.map do |station|
-          #{
-            #type: 'Feature',
-            #geometry: {
-              #type: 'Point',
-              #coordinates: [station.xcoord, station.ycoord]
-            #},
-            #properties: {
-              #name: station.name,
-              #address: station.address
-            #}
-          #}
-        #end
-    
-        #print geojson
         render json: geojson
-      #end
-    #end
   end
 
 
@@ -58,37 +28,6 @@ class StationsController < ApplicationController
         render json: geojson
 
   end
-
-
-=begin
-require 'geojson_model'
-require 'json'
-
-
-
-def index
-
-  @stations = Station.all
-
-  locations = []
-
-  @stations.map do |station|
-  
-    puts station.name
-
-    locations << GeojsonModel::Feature.new(
-        properties: {name: station.name},
-        geometry: GeojsonModel::Geometry.new(type: 'Point', coordinates: [station.xcoord, station.ycoord])
-        )
-  end
-
-  fc = GeojsonModel::FeatureCollection.new(features: locations)
-  puts JSON.pretty_generate(JSON.parse(fc.to_json)))
-  render json: JSON.pretty_generate(JSON.parse(fc.to_json)))
-
-  end
-
-=end
 
 end
 
