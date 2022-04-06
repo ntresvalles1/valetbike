f7d47fef3e12db2f8235c7719c80319fffece84b
elainezhang <ezzhang@smith.edu>
2022-04-06 18:55:59 -0400

    working sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,11 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       log_in @user
-      redirect_to user
+      flash[:success] = "Welcome to Bikea"
+      redirect_to @user
     else
       render 'new'
     end
