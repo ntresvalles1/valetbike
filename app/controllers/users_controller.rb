4d21eaf7154de65d48ae25b878785150c6f538e6
Nicole Tresvalles <nicoletresvalles01@gmail.com>
2022-04-19 16:31:18 -0400

    improved front-end aesthetic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,12 @@ class UsersController < ApplicationController
   end
   
   def show
-# <<<<<<< HEAD
-#     @user = User.find(params[:id])
-# =======
+
     if !session[:user_id]
       redirect_to login_path
     else
       @user = User.find(session[:user_id])
     end
-# >>>>>>> user-profile-updated
   end
   
   def new
@@ -22,19 +19,12 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-# <<<<<<< HEAD
 
     if @user.save
       UserMailer.account_activation(@user).deliver_now     
       flash[:warning] = "Please check your email to activate your account."
       redirect_to root_url
-# =======
-#     if @user.valid?
-#       @user.save
-#       log_in @user
-#       flash[:success] = "Welcome to Bikea"
-#       redirect_to @user
-# >>>>>>> user-profile-updated
+
     else
       render 'new'
     end
@@ -55,13 +45,7 @@ class UsersController < ApplicationController
     else
       render 'editProfile'
     end
-    # if @user.update(params.require(:user).permit(:username, :email, :password, :password_confirmation, :first_name, :last_name))
-    #   flash[:success] = "User information successfully updated"
-    #   redirect_to  @user #todo_url(@user)
-    # else
-    #   flash.now[:error] = "User information update failed"
-    #   render :editProfile
-    # end
+    
   end
 
 
