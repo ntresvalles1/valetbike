965e5f6c353f741457fe1e996234b3aa93c936a2
elainezhang <ezzhang@smith.edu>
2022-04-20 22:04:32 -0400

    delete commented code

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,18 +1,8 @@
 class Ride < ApplicationRecord
     validates_presence_of   :bike_id,
                             :startstation
-                            #:rider_user_id
-                            #:identifier,
-                            #:starttime
-                            #:endstation
-                            #:endtime
-                            #:membershipid
                             
 
     belongs_to :rider, class_name: :User, foreign_key: :rider_user_id, optional: true
-    #validates_uniqueness_of :identifier 
-
-    #belongs_to: memebrship
-    #belongs_to  :user
 
 end
